4f52e0c092ba50e36f3b93bcd5407a86e982f4fb
PushpikaWan <lucefer41@gmail.com>
2019-07-28 12:30:59 +0530

    file structure changed -ignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { DashboardControlComponent } from './dashboard-control/dashboard-control.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardControlComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  counter = 12;
+  horizontalLists = [];
+  connectedTo = [];
+
+  constructor()
+  {
+    this.initaliseDashboard(); 
+  }
+
+  private initaliseDashboard()
+  {
+    this.horizontalLists = [
+      {
+        id:'list_0',
+        cardList:[
+          "card_01",
+          "card_02",
+          "card_03"
+        ]
+      },
+      {
+        id:'list_1',
+        cardList:[
+          "card_11",
+          "card_12",
+          "card_13"
+        ]
+      },
+      {
+        id:'list_2',
+        cardList:[
+          "card_21",
+          "card_22",
+          "card_23"
+        ]
+      }
+    ];
+
+    for (let cardList of this.horizontalLists) {
+      this.connectedTo.push(cardList.id);
+    };
+  }
+
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  addHorizontalList(){
+    this.horizontalLists.push([this.counter]);
+    this.counter++;
+  }
+
+}

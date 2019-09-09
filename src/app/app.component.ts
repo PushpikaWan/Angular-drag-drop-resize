e7fe617771d396fbaab414a6b3632d7330e816ae
PushpikaWan <lucefer41@gmail.com>
2019-09-09 23:51:07 +0530

    resize conflicting resolve logic partially added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,8 @@ export class AppComponent {
       {id: 2, xStart: 2, xEnd: 4, yStart: 1, yEnd: 3},
       {id: 3, xStart: 4, xEnd: 5, yStart: 1, yEnd: 2},
       {id: 4, xStart: 1, xEnd: 2, yStart: 2, yEnd: 5},
-      {id: 5, xStart: 2, xEnd: 3, yStart: 3, yEnd: 5}
+      {id: 5, xStart: 2, xEnd: 3, yStart: 3, yEnd: 5},
+      {id: 6, xStart: 3, xEnd: 4, yStart: 3, yEnd: 5}
     ];
   }
 
@@ -24,13 +25,36 @@ export class AppComponent {
     console.log(this.items[index], newColumnValue);
     let xEndOld =  this.items[index].xEnd ;
     let xEndNew =  this.items[index].xStart + this.toInt(newColumnValue);
-    this.moveConflictingColumns(xEndOld, xEndNew);
+    this.moveConflictingColumns(xEndOld, xEndNew, this.items[index]);
+    //todo index use may introduce error use id instead of
     this.items[index].xEnd = xEndNew;
   }
 
-  private moveConflictingColumns(xEndOld: number, xEndNew: number) {
-
+  /**
+   * todo - simplify this logic
+   * This function is used to arrange other conflicting blocks when resizing elements
+   * @param xEndOld
+   * @param xEndNew
+   * @param resizingItem
+   */
+  private moveConflictingColumns(xEndOld: number, xEndNew: number, resizingItem: DashboardItem) {
+    this.items
+      .filter(val => !(val.xStart < resizingItem.xEnd || val.yEnd <= resizingItem.yStart || val.yStart >= resizingItem.yEnd))
+      .sort((n1,n2) => n1.xStart - n2.xStart)
+      .forEach((val,index) => {
+        if(xEndNew > val.xStart){
+           let diff = xEndNew - val.xStart;
+           val.xStart += diff;
+           val.xEnd += diff;
+           //recursion
+           return this.moveConflictingColumns(val.xEnd - diff, val.xEnd,val);
+        }
+        else{
+          return;
+        }
+      })
   }
+
 
   private toInt(val: any, fallbackValue: number = 0): number {
     const normalized = String(val).replace(/[\D]/g, '');

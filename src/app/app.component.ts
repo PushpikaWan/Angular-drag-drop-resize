303d0d9aa2dd4337494bbf7cdd11bf8ef1838ec5
PWanniachchi <Pushpika.Wanniachchi@cambio.lk>
2019-09-10 19:35:57 +0530

    html drag function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { DashboardControllingService } from './dashboard-controlling.service';
 
 @Component({
@@ -6,11 +6,15 @@ import { DashboardControllingService } from './dashboard-controlling.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// todo - heights are difference when for same row sizes
+
 export class AppComponent {
   title = 'angular-drag-drop-resize';
   items: DashboardItem[];
+  dragSrcEl = null;
 
-  constructor( dashboardControllingService: DashboardControllingService) {
+  constructor(private dashboardControllingService: DashboardControllingService, private renderer: Renderer2) {
     this.items = [
       {id: 1, xStart: 1, xEnd: 2, yStart: 1, yEnd: 2},
       {id: 2, xStart: 2, xEnd: 4, yStart: 1, yEnd: 3},
@@ -22,13 +26,13 @@ export class AppComponent {
   }
 
   updateCols(index: number, newColumnValue: any) {
-    //todo index use may introduce error, use id instead of
+    // todo index use may introduce error, use id instead of
     this.items[index].xEnd = this.items[index].xStart + this.toInt(newColumnValue);
     this.moveConflictingColumns(this.items[index]);
   }
 
   updateRows(index: number, newRowValue: any) {
-    //todo index use may introduce error, use id instead of
+    // todo index use may introduce error, use id instead of
     this.items[index].yEnd = this.items[index].yStart + this.toInt(newRowValue);
     this.moveConflictingRows(this.items[index]);
   }
@@ -61,14 +65,67 @@ export class AppComponent {
         val.yStart += diff;
         val.yEnd += diff;
         // recursion
-        return this.moveConflictingColumns(val);
+        return this.moveConflictingRows(val);
       });
   }
+
+  handleDragStart(event) {
+    console.log('drag start');
+    this.renderer.setStyle(event.target, 'opacity', '0.4');
+    this.dragSrcEl = this;
+
+    event.dataTransfer.effectAllowed = 'move';
+    // event.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  handleDragEnd(event) {
+    console.log('drag stop');
+    this.renderer.setStyle(event.target, 'opacity', '1.0');
+  }
+
+  handleDragOver(event) {
+    if (event.preventDefault) {
+      console.log('drag over', event);
+      event.preventDefault(); // Necessary. Allows us to drop.
+    }
+    event.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    return false;
+  }
+
+  handleDragEnter(event) {
+    console.log('drag enter');
+    // this / e.target is the current hover target.
+    // this.classList.add('over');
+  }
+
+  handleDragLeave(event) {
+    console.log('drag leave');
+   // this / e.target is previous target element.
+    // this.classList.remove('over');
+  }
+
+  handleDrop(e) {
+    // this/e.target is current target element.
+
+    if (e.stopPropagation) {
+      e.stopPropagation(); // Stops some browsers from redirecting.
+    }
+
+    // Don't do anything if dropping the same column we're dragging.
+    if (this.dragSrcEl !== this) {
+      // Set the source column's HTML to the HTML of the column we dropped on.
+      // this.dragSrcEl.innerHTML = this.innerHTML;
+      // this.innerHTML = e.dataTransfer.getData('text/html');
+      console.log('drop into');
+    }
+
+    return false;
+  }
+
 
   private isConflictingItem(resizingItem: DashboardItem, item: DashboardItem): boolean {
     return item.xStart < resizingItem.xEnd && item.xEnd > resizingItem.xStart && item.yStart < resizingItem.yEnd && item.yEnd > resizingItem.yStart && item.id !== resizingItem.id;
   }
-
 
   private toInt(val: any, fallbackValue: number = 0): number {
     const normalized = String(val).replace(/[\D]/g, '');

366d957357870c0b7826af6e4a743dfc105fb6c7
PWanniachchi <Pushpika.Wanniachchi@cambio.lk>
2019-09-17 13:58:00 +0530

    media queries added to display between 1100- 540 px

--- projects/angular-drag-drop-rearrange/src/lib/dashboard-view/dashboard-view.component.ts
+++ projects/angular-drag-drop-rearrange/src/lib/dashboard-view/dashboard-view.component.ts
@@ -1,19 +1,19 @@
-import {Component, HostListener, OnInit, Renderer2} from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { DashboardControllingService } from '../services/dashboard-controlling.service';
 import { DashboardItem } from '../dashboard-item.model';
 
 @Component({
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-dashboard-view',
   templateUrl: './dashboard-view.component.html',
   styleUrls: ['./dashboard-view.component.css']
 })
 export class DashboardViewComponent {
   dashboardItems: Map<number, DashboardItem> = new Map();
-  isMinimized = false;
+  columnCountPerRow = 8; // 1 -> (0 -540) 2-> (541- 900) 8 -> (900>)
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    console.log('resize event:', event.target.innerWidth);
     this.triggerResizeWindow(event.target.innerWidth, event.target.innerHeight);
   }
 
@@ -32,15 +32,18 @@ export class DashboardViewComponent {
 
   private triggerResizeWindow(width: number, height: number) {
     // todo check height and update resize event at initial time
-    if (width <= 376) {
-      this.dashboardControllingService.updateMaxColumnCountWhenResize(1);
-      this.isMinimized = true;
-      console.log('minimized');
-    } else{
-      this.dashboardControllingService.updateMaxColumnCountWhenResize(8);
-      this.isMinimized = false;
-      console.log('max', 'width', width);
+    if (width <= 540) {
+      this.columnCountPerRow = 1;
+    } else if (width <= 1100) {
+      this.columnCountPerRow = 2;
+    } else {
+      this.columnCountPerRow = 8;
     }
+    console.log('width', width);
+    this.dashboardControllingService.updateMaxColumnCountWhenResize(this.columnCountPerRow);
+  }
+  getFloorAfterDividedByTwo( value: number): number {
+    return Math.floor(value / 2);
   }
   updateCols(index: number, newColumnValue: any) {
     this.dashboardControllingService.updateCols(index, newColumnValue);

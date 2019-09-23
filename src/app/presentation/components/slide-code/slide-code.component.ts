94f6d74c9483fc1d0488e79b00b4702dbe8c6eab
PushpikaWan <lucefer41@gmail.com>
2019-09-23 22:05:05 +0530

    angular presentation draft added using https://github.com/beeman/angular-presentations

---
+++ src/app/presentation/components/slide-code/slide-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { AbstractSlideComponent } from '../abstract-slide/abstract-slide.component'
+
+@Component({
+  selector: 'app-slide-code',
+  template: `
+    <app-abstract-slide [content]="content">
+<!--      <prism *ngIf="content.code && content.language" [code]="content.code" [language]="content.language"></prism>-->
+    </app-abstract-slide>
+  `,
+})
+export class SlideCodeComponent extends AbstractSlideComponent {}
